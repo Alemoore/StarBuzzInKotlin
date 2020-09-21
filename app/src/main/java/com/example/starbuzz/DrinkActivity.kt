df12103fdd9b49e39f36e224c2d8c3eda6d4eb66
alemoore <alexey.murnikoff@yandex.ru>
2020-09-21 18:25:34 +0300

    CursorAdapter was added to DrinkCategoryActivity

--- app/src/main/java/com/example/starbuzz/DrinkActivity.kt
+++ app/src/main/java/com/example/starbuzz/DrinkActivity.kt
@@ -1,7 +1,11 @@
 package com.example.starbuzz
 
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_drink.*
 
 class DrinkActivity : AppCompatActivity() {
@@ -9,10 +13,23 @@ class DrinkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink)
         val position = intent.extras!!.get(DRINKID) as Int
-        val drink = DrinkManager.drinks[position]
-        photo.setImageResource(drink.image)
-        drink_name.text = drink.name
-        drink_description.text = drink.description
+        val starbuzzDatabaseHelper: SQLiteOpenHelper = StarbuzzDatabaseHelper(this)
+        try {
+            val db: SQLiteDatabase = starbuzzDatabaseHelper.readableDatabase
+            val cursor = db.query("DRINK", arrayOf("NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"), "_id=?", arrayOf((position + 1).toString()), null, null, null)
+            if (cursor.moveToFirst()) {
+                val nameText = cursor.getString(0)
+                val descriptionText = cursor.getString(1)
+                val imageResourceId = cursor.getInt(2)
+                drink_name.text = nameText
+                drink_description.text = descriptionText
+                photo.setImageResource(imageResourceId)
+                cursor.close()
+                db.close()
+            }
+        } catch (e: SQLiteException) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.kt
+++ app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.kt
@@ -1,22 +1,44 @@
 package com.example.starbuzz
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_drink_category.*
 
 class DrinkCategoryActivity : AppCompatActivity() {
+    private lateinit var db: SQLiteDatabase
+    private lateinit var cursor: Cursor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink_category)
-        val arrayAdapter =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, DrinkManager.drinks)
-        drinks_list.adapter = arrayAdapter
+        try {
+            val starbuzzDatabaseHelper: SQLiteOpenHelper = StarbuzzDatabaseHelper(this)
+            db = starbuzzDatabaseHelper.readableDatabase
+            cursor = db.query("DRINK", arrayOf("_id", "Name"), null, null, null, null, null)
+            val listAdapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, arrayOf("NAME"), intArrayOf(android.R.id.text1), 0)
+            drinks_list.adapter = listAdapter
+        } catch (e: SQLiteException) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show()
+        }
+
         drinks_list.setOnItemClickListener { parent, view, position, id ->
             val intent = Intent(this, DrinkActivity::class.java)
             intent.putExtra(DrinkActivity.DRINKID, position)
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cursor.close()
+        db.close()
     }
 }
